347967e931a347182fb37aba4509c3eaea82a507
Igor Barakaiev <ibarakaiev@gmail.com>
2024-06-26 23:23:20 +0300

    check uniqueness when adding to list

--- lib/mix/tasks/ash_money.add_to_ash_postgres.ex
+++ lib/mix/tasks/ash_money.add_to_ash_postgres.ex
@@ -1,5 +1,5 @@
 defmodule Mix.Tasks.AshMoney.AddToAshPostgres do
-  @moduledoc "Adds AshMoney.AshPostgresExtension to installed_extensions. Should be run as an Igniter task."
+  @moduledoc "Adds AshMoney.AshPostgresExtension to installed_extensions and installs :ex_money_sql."
   @shortdoc @moduledoc
   require Igniter.Code.Common
   use Igniter.Mix.Task
@@ -10,13 +10,12 @@ defmodule Mix.Tasks.AshMoney.AddToAshPostgres do
     repo_path = Igniter.Code.Module.proper_location(repo)
 
     igniter
-    # TODO: remove fixed version
-    |> Igniter.Project.Deps.add_dependency(:ex_money_sql, "~> 1.11")
+    |> Igniter.Project.Deps.add_dependency(:ex_money_sql, "~> 1.0")
     |> Igniter.add_task("deps.get")
     |> Igniter.update_elixir_file(repo_path, fn zipper ->
       with {:ok, zipper} <- Igniter.Code.Module.move_to_module_using(zipper, AshPostgres.Repo),
            {:ok, zipper} <- Igniter.Code.Module.move_to_def(zipper, :installed_extensions, 0) do
-        Igniter.Code.List.append_to_list(zipper, quote(do: AshMoney.AshPostgresExtension))
+        Igniter.Code.List.append_new_to_list(zipper, quote(do: AshMoney.AshPostgresExtension))
       else
         _ ->
           Igniter.add_issue(
