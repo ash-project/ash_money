de8dac0194644bf5fb80bf5476213a41eb3fe668
Zach Daniel <zach@zachdaniel.dev>
2024-07-14 22:24:10 -0400

    chore: format

--- mix.exs
+++ mix.exs
@@ -117,7 +117,7 @@ defmodule AshMoney.MixProject do
       {:sobelow, ">= 0.0.0", only: [:dev, :test], runtime: false},
       {:git_ops, "~> 2.5", only: [:dev, :test]},
       {:mix_test_watch, "~> 1.0", only: :dev, runtime: false},
-      {:mix_audit, ">= 0.0.0", only: [:dev, :test], runtime: false},
+      {:mix_audit, ">= 0.0.0", only: [:dev, :test], runtime: false}
     ]
   end
 
