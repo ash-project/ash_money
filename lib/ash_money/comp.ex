96f6e3065de384e190b287d327eadd0e88a4c9c2
James Harton <james@harton.nz>
2025-10-10 13:48:37 +1300

    chore: REUSE compliance

--- lib/ash_money/comp.ex
+++ lib/ash_money/comp.ex
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2020 Zach Daniel
+#
+# SPDX-License-Identifier: MIT
+
 import Ash.Type.Comparable
 
 defcomparable left :: Money, right :: Integer do
--- lib/mix/tasks/ash_money.install.ex
+++ lib/mix/tasks/ash_money.install.ex
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2020 Zach Daniel
+#
+# SPDX-License-Identifier: MIT
+
 if Code.ensure_loaded?(Igniter) do
   defmodule Mix.Tasks.AshMoney.Install do
     @moduledoc "Installs AshMoney. Should be run with `mix igniter.install ash_money`"
