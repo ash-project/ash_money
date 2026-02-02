95c21be25b5c76d17997a51c3ee9fa9ae59bbd25
Leon Qadirie <39130739+leonqadirie@users.noreply.github.com>
2026-02-02 07:11:44 +0100

    chore: SPDX contributor links (#166)

--- lib/ash_money/comp.ex
+++ lib/ash_money/comp.ex
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2023 ash_money contributors <https://github.com/ash-project/ash_money/graphs.contributors>
+# SPDX-FileCopyrightText: 2023 ash_money contributors <https://github.com/ash-project/ash_money/graphs/contributors>
 #
 # SPDX-License-Identifier: MIT
 
